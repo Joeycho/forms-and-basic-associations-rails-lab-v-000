4cd0da94c0b187443bb134aae08fdcbc946a9c83
Joey Cho <cjy12182000@naver.com>
2019-02-22 10:33:52 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,45 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |c|
+      if c == nil || c==""
+        errors.add(:notes, "Invalid content")
+      else
+        @note = Note.find_or_create_by(content: c)
+        self.notes << @note
+      end
+    end
+
+  end
+
+  def note_contents
+    if !self.notes.empty?
+      self.notes.collect do |n|
+        n.content
+      end
+    else
+      errors.add(:notes, "No note in the song")
+    end
+  end
+
 end
